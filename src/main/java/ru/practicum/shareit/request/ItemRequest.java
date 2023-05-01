1629c1b09e25585a51bb8c26bdb521f006b2f5a9
AlexF0890 <98687927+AlexF0890@users.noreply.github.com>
2023-05-01 23:51:24 +0300

    feat:Создать 13.1 спринт

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -2,19 +2,19 @@ package ru.practicum.shareit.request;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.practicum.shareit.user.User;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-/**
- * TODO Sprint add-item-requests.
- */
 @Data
 @NoArgsConstructor
 public class ItemRequest {
     private Long id;
+
     @NotNull
     private String description;
+
     @NotNull
     private User requestor;
     private LocalDateTime created;
